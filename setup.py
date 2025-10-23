b7093228c801c85149371f5fa4e34f0052463651
Atharva2223 <atharvapatade22@gmail.com>
2025-10-22 22:22:44 -0700

    requirement added

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,find_packages
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Atharva",
+    author_email="atharvapatade22@gmail.com",
+    packages=find_packages()
+)
